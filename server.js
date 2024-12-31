f3c5ded759c0e11f8157f1fc5ecf634d31fb3a7c
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-12-31 21:53:03 +0530

    server cors

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
+    credentials: true, // Allows the sending of cookies and HTTP authentication information
   })
 );
 
